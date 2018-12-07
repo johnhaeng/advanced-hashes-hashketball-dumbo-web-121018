d07347a59a987f42d76c4b99cb9d614f04611983
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-07 04:11:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,24 @@
 # Write your code here!
 
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        :Alan_Anderson => {:numbers => , :shoe => "nike", :points => "35", :rebounds => "5", :assists => "5", :steals => "2", :blocks => "1", :slam_dunks => "2"},
+        :Reggie_Evanse => {"32", "13", "4"},
+        :
+      }
+    },
+    
+    :away => {
+      :team_name,
+      :colors,
+      :players
+    }
+  }
+end
 
 
 
